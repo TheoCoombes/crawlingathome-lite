1967b6964da738caaf836e89c3a7dae0aa9d8876
Theo Coombes <theocoombes06@gmail.com>
2021-06-06 03:31:21 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "[crawling@home-lite] starting script..."
-screen -dm bash -c 'python3 crawlfulltest.py'
+screen -dm bash -c 'python3 main.py'
 echo "[crawling@home-lite] script started"
 screen -ls
 echo "[crawling@home-lite] use `screen -r <ID SHOWN ABOVE>` to view your worker's progress."
