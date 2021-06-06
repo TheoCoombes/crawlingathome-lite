5100dc62b1ed8e4c363fbee364e3bd27557816f9
Theo Coombes <theocoombes06@gmail.com>
2021-06-06 03:38:43 +0100

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 echo "[crawling@home-lite] starting script..."
+
+ulimit -n 65356 # Verify the amount of open files possible is high.
 screen -dm bash -c 'python3 main.py'
+
 echo "[crawling@home-lite] script started"
+
 screen -ls
+
 echo "[crawling@home-lite] use `screen -r <ID SHOWN ABOVE>` to view your worker's progress."
